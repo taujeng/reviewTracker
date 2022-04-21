e13d3dcacdf6340d788981839a40a41a3bd33513
Tau <taujeng.1@gmail.com>
2022-04-21 21:15:22 +0800

    Initial Commit

---
+++ src/components/Post.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+const Post = () => {
+  return (
+    <div className="post-container">
+      <div className="post-title post">Taco John's</div>
+      <div className="post-rating post">star star star</div>
+      <div className="post-body post">So good!</div>
+      <div className="post-footer">
+        <div className="post-footer-name">John Doe</div>
+        <div className="post-footer-date">01/01/2019</div>
+        <div className="post-footer-comments">ICON</div>
+      </div>
+    </div>
+  );
+};
+
+export default Post;
---
+++ src/components/Reviews.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import Post from './Post'
+
+const Reviews = () => {
+  return (
+    <section className='review-container'>
+      <Post />
+      <Post />
+      <Post />
+      <Post />
+      <Post />
+      <Post />
+    </section>
+  )
+}
+
+export default Reviews
