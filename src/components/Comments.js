567d0626ef3ff0ea1de1e050e24d3258ffa2b2d9
Tau <taujeng.1@gmail.com>
2022-04-23 23:22:08 +0800

    Created dynamic Comments page, begun styling

--- src/components/Comments.js
+++ src/components/Comments.js
@@ -1,16 +1,25 @@
 import React from 'react';
-import Post from './Post';
+import StarRating from './StarRating';
 import { useLocation } from 'react-router-dom';
 
 const Comments = () => {
   const location = useLocation();
   const { from } = location.state;
-  console.log(from);
-  console.log(from.details);
-  console.log(from.updateDate);
+  // from.details = data, from.updateDate = updated Date from data
+
   return (
     <div className="comments-container">
-      <div className="comments-box original-review">{from.details.author}</div>
+      <div className="comments-box comments-review">
+        <div className="comments-post-title">{from.details.place}</div>
+        <div className="comments-post-rating">
+          <StarRating score={from.details.rating} />
+        </div>
+        <div className="comments-post-body">{from.details.content}</div>
+        <div className="comments-footer">
+          <div className="comments-footer-name">Tau</div>
+          <div className="comments-footer-date">4/23/2022</div>
+        </div>
+      </div>
       <div className="comments-box comments-response">Response</div>
     </div>
   );
