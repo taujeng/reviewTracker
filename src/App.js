cc8dd2c47350b908617c45c3ac5f8115975d6d2b
Tau <taujeng.1@gmail.com>
2022-04-23 10:19:28 +0800

    Each post has dynamic date, review, and star rating

--- src/App.js
+++ src/App.js
@@ -1,13 +1,19 @@
 import Header from './components/Header';
 import Body from './components/Body';
+import Comments from './components/Comments'
+import { Routes, Route, Link } from "react-router-dom";
 
 function App() {
   return (
     <div className="App">
       <Header />
-      <Body />
+      <Routes>
+        <Route path="/" element={<Body />}/>
+        <Route path="/comments" element={<Comments />}/>
+      </Routes>
     </div>
   );
 }
 
 export default App;
+
--- src/components/Post.js
+++ src/components/Post.js
@@ -1,17 +1,57 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import StarRating from './StarRating';
+import { Link } from 'react-router-dom';
 
-const Post = ({ author }) => {
+const Post = ({ details }) => {
+  // function newDate(details) {
+  //   const oldDate = details['published_at'].slice(4, 15);
+  //   return oldDate;
+  // }
+
+  // Reformat Date
+  // date we receive: "Tue Feb 26 2008 20:52:20 GMT-0600 (Central Standard Time)"
+  // date we want: 02/24/1986
+  const [updateDate, setUpdateDate] = useState(details.published_at);
+  const months = {
+    Jan: '01',
+    Feb: '02',
+    Mar: '03',
+    Apr: '04',
+    May: '05',
+    Jun: '06',
+    Jul: '07',
+    Aug: '08',
+    Sep: '09',
+    Oct: '10',
+    Nov: '11',
+    Dec: '12',
+  };
+
+
+
+  useEffect(() => {
+    const oldDate = details['published_at'].slice(4, 15).replaceAll(' ', '/');
+    for (const [key, value] of Object.entries(months)) {
+      if (oldDate.includes(key)) {
+        const newDate = oldDate.replace(key, value);
+        setUpdateDate(newDate);
+      }
+    }
+  }, []);
+
   return (
-    <div className="post-container">
-      <div className="post-title post">Taco John's</div>
-      <div className="post-rating post">star star star</div>
-      <div className="post-body post">So good!</div>
+    <Link className="post-container" to={'/comments/' + details.id}>
+      <div className="post-title post">{details.place}</div>
+      <div className="post-rating post">
+        <StarRating score={details.rating} />
+      </div>
+      <div className="post-body post">{details.content}</div>
       <div className="post-footer">
-        <div className="post-footer-name">{author}</div>
-        <div className="post-footer-date">01/01/2019</div>
+        <div className="post-footer-name">{details.author}</div>
+        <div className="post-footer-date">{updateDate}</div>
         <div className="post-footer-comments">ICON</div>
       </div>
-    </div>
+    </Link>
   );
 };
 
