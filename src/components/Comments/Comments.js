c63c91836ebd2db42cca17302c1e5b2ba31aa88c
Tau <taujeng.1@gmail.com>
2022-04-28 00:49:12 +0800

    CSS: made containers more responsive

--- src/components/Comments/Comments.js
+++ src/components/Comments/Comments.js
@@ -3,7 +3,6 @@ import StarRating from '../StarRating/StarRating';
 import { useLocation } from 'react-router-dom';
 import { Reply, MoreHoriz, RefreshSharp } from '@mui/icons-material';
 import { Link } from 'react-router-dom';
-import { render } from '@testing-library/react';
 import './comments.css'
 
 const Comments = ({ response }) => {
